f8527911887c854fb2a241bb2bf102fb5684ad64
dakota-bayer <dakota.bayer@gmail.com>
2022-04-14 19:59:59 -0500

    Finish video 6.

--- MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,14 +15,8 @@ namespace MyCoffeeApp.Views
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
-            LabelCount.Text = "Hello from the Code Behind!";
         }
 
-        int count = 0;
-        private void ButtonClick_Clicked(object sender, EventArgs e)
-        {
-            count++;
-            LabelCount.Text = $"You clicked {count} time(s).";
-        }
+
     }
 }
